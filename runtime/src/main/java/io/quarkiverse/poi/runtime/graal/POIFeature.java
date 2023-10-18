a0006db425306bdc1d34738b837c1421497c1c52
George Gastaldi <gegastaldi@gmail.com>
2023-10-18 12:28:39 -0300

    Prevent IAE when accessing GraalVM classes
    Caused by: java.lang.IllegalAccessError: class io.quarkiverse.poi.runtime.graal.POIFeature (in unnamed module @0x127e70c5) cannot access class org.graalvm.nativeimage.impl.RuntimeClassInitializationSupport (in module org.graalvm.nativeimage) because module org.graalvm.nativeimage does not export org.graalvm.nativeimage.impl to unnamed module @0x127e70c5

--- runtime/src/main/java/io/quarkiverse/poi/runtime/graal/POIFeature.java
+++ runtime/src/main/java/io/quarkiverse/poi/runtime/graal/POIFeature.java
@@ -1,18 +1,16 @@
 package io.quarkiverse.poi.runtime.graal;
 
-import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.hosted.Feature;
-import org.graalvm.nativeimage.impl.RuntimeClassInitializationSupport;
+import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
 
 public class POIFeature implements Feature {
     @Override
     public void afterRegistration(AfterRegistrationAccess access) {
-        final RuntimeClassInitializationSupport runtimeInit = ImageSingletons.lookup(RuntimeClassInitializationSupport.class);
         final String reason = "Quarkus run time init for Apache POI";
-        runtimeInit.initializeAtRunTime("org.apache.poi.hssf.util", reason);
-        runtimeInit.initializeAtRunTime("org.apache.poi.ss.format", reason);
-        runtimeInit.initializeAtRunTime("org.apache.poi.util.RandomSingleton", reason);
-        runtimeInit.initializeAtRunTime("org.apache.poi.ss.util.SheetUtil", reason);
+        RuntimeClassInitialization.initializeAtRunTime("org.apache.poi.hssf.util", reason);
+        RuntimeClassInitialization.initializeAtRunTime("org.apache.poi.ss.format", reason);
+        RuntimeClassInitialization.initializeAtRunTime("org.apache.poi.util.RandomSingleton", reason);
+        RuntimeClassInitialization.initializeAtRunTime("org.apache.poi.ss.util.SheetUtil", reason);
     }
 
     @Override
